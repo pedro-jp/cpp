921aa69279a02a9a13ade3a17f38dfa4eb0f1f9c
pedro-jp <joaopedroc035@gmail.com>
2025-01-10 22:41:38 -0300

    studies: listas

--- listas.cpp
+++ listas.cpp
@@ -6,14 +6,30 @@ using namespace std;
 int main()
 {
   list<int> aula;
+  list<int> aula2;
   list<int>::iterator it;
 
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 5; i++)
   {
     aula.push_front(i);
   }
 
+  for (int i = 10; i > 5; i--)
+  {
+    aula2.push_front(i);
+  }
+
+  // aula.reverse();
+  aula.merge(aula2);
+  aula.sort();
+
   it = aula.begin();
+
+  advance(it, 3);
+  aula.insert(it, 10);
+  aula.erase(--it);
+
+  // aula.clear();
 
   while (!aula.size() <= 0)
   {
